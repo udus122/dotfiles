862f5b578e796aad202745fa5089607be99a43a8
yusuda <yusuda@yahoo-corp.jp>
2024-01-17 12:39:42 +0900

    ディレクトリを自動作成

--- link.sh
+++ link.sh
@@ -6,11 +6,15 @@ SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 function make_link () {
     src=$1
     dst=$2
+
     # $dst がシンボリックリンクでない場合は終了
     if [[ -e $dst && ! -L $dst ]]; then
         echo "$dst is not a symbolic link"
         return 1
     fi
+    # ディレクトリが存在しない場合は作成
+    dirpath=$(dirname "$dst")
+    [[ ! -d "$dirpath" ]] && mkdir -p "$dirpath"
 
     ln -fnsv $src $dst
 }
@@ -19,6 +23,6 @@ find "${SCRIPT_DIR}" -type f -path '*/.*' | while read dotfile; do
     [[ "$dotfile" == "${SCRIPT_DIR}/.git"* ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.github"* ]] && continue
     [[ "$dotfile" == "${SCRIPT_DIR}/.DS_Store" ]] && continue
-    
+
     make_link "$dotfile" ${HOME}/${dotfile#$SCRIPT_DIR/}
 done
