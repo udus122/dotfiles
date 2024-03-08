fbb1aab1682b46f005055cb03a364467e9139eed
udus122 <u.yudai1202@gmail.com>
2024-03-08 10:53:57 +0900

    Update ls abbreviations to include color option

--- .config/zsh/abbreviations.zsh
+++ .config/zsh/abbreviations.zsh
@@ -16,11 +16,11 @@ abbr -S 'kn'='kubens' > /dev/null
 abbr -S 'gc'='gcloud' > /dev/null
 abbr -S 'gccl'='gcloud config configurations list' > /dev/null
 abbr -S 'gco'='open "https://console.cloud.google.com/home/dashboard?project=$(gcloud config get-value project)"' > /dev/null
-abbr -S 'l'='ls -FG' > /dev/null
-abbr -S 'ls'='ls -FG' > /dev/null
-abbr -S 'la'='ls -FGa' > /dev/null
-abbr -S 'll'='ls -FGl' > /dev/null
-abbr -S 'lla'='ls -FGla' > /dev/null
+abbr -S 'l'='ls -F --color=auto' > /dev/null
+abbr -S 'ls'='ls -F --color=auto' > /dev/null
+abbr -S 'la'='ls -FA --color=auto' > /dev/null
+abbr -S 'll'='ls -Fl --color=auto' > /dev/null
+abbr -S 'lla'='ls -FlA--color=auto' > /dev/null
 abbr -S 'r'="mise run --" > /dev/null
 abbr -S 'x'="mise exec --" > /dev/null
 abbr -S 'reload'="exec -l $SHELL" > /dev/null
