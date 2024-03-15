1610c44d877854fe4b4b0f3c4500bad5977628f2
udus122 <u.yudai1202@gmail.com>
2024-03-15 15:24:31 +0900

    Update key assignments and add leader key in wezterm.lua

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -17,13 +17,24 @@ config.use_ime = true
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = true
 
+-- leader
+config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 2000 }
+
 -- key assignment
 config.keys = {
-    {
-        key = 'p',
-        mods = 'SUPER|SHIFT',
-        action = wezterm.action.ActivateCommandPalette
-    },
+    { key = 'p', mods = 'SUPER|SHIFT', action = wezterm.action.ActivateCommandPalette },
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key="LeftArrow", mods="OPT", action=wezterm.action{ SendString="\x1bb" } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key="RightArrow", mods="OPT", action=wezterm.action{ SendString="\x1bf" } },
+    { key = '"', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = '%', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+    { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
+    { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
+    { key = '[', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+    { key = 'a', mods = 'LEADER|CTRL', action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' } },
     { key = 'UpArrow', mods = 'SHIFT', action = wezterm.action.ScrollToPrompt(-1) },
     { key = 'DownArrow', mods = 'SHIFT', action = wezterm.action.ScrollToPrompt(1) },
 }
