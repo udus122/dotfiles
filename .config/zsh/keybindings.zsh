49ab65e313df746eb3dc0fb434ed91a94b029a98
udus122 <u.yudai1202@gmail.com>
2024-10-21 09:59:43 +0900

    fzf-git.shを導入し、既存のfzfとgitの補完設定を削除する

--- .config/zsh/keybindings.zsh
+++ .config/zsh/keybindings.zsh
@@ -11,7 +11,7 @@ bindkey -M viins 'jj' vi-cmd-mode
 bindkey -M viins '^D' delete-char-or-list
 bindkey -M viins '^E' end-of-line
 bindkey -M viins '^F' forward-char
-bindkey -M viins '^G' send-break
+# bindkey -M viins '^G' send-break
 bindkey -M viins '^H' backward-delete-char
 bindkey -M viins '^K' kill-line
 bindkey -M viins '^N' down-line-or-history
@@ -85,35 +85,3 @@ bindkey '^[[1;2D' __cd_hist_fzf
 # shift+rightでfzf-cd-widgetを呼び出す
 # カレントディレクトリから選択して移動
 bindkey '^[[1;2C' fzf-cd-widget
-# ---------------------------------------------------
-
-# git関連
-
-# ctrl+g ctrl+wでブランチ切り替え
-# ref. https://zenn.dev/yamo/articles/5c90852c9c64ab
-__git_sw() {
-  target_br=$(
-    git branch -a |
-      fzf --exit-0 --layout=reverse --info=hidden --no-multi --preview-window="right,65%" --prompt="CHECKOUT BRANCH > " --preview="echo {} | tr -d ' *' | xargs git log --graph --decorate --abbrev-commit --format=format:'%C(blue)%h%C(reset) - %C(green)(%ar)%C(reset)%C(yellow)%d%C(reset)\n  %C(white)%s%C(reset) %C(dim white)- %an%C(reset)' --color=always" |
-      head -n 1 |
-      perl -pe "s/\s//g; s/\*//g; s/remotes\/origin\///g"
-  )
-  if [ -n "$target_br" ]; then
-    BUFFER="git switch $target_br"
-    zle accept-line
-  fi
-}
-zle -N __git_sw
-bindkey -r '^g^w'
-bindkey '^g^w' __git_sw
-
-# ctrl+g ctrl+sでgit status
-__git_status() {
-  if [ "$(git rev-parse --is-inside-work-tree 2> /dev/null)" = 'true' ]; then
-    echo git status -sb # git statusを実行したっぽくみせかける
-    git status -sb
-  fi
-  zle accept-line
-}
-zle -N __git_status  # _git_status関数をgit_status widgetとして登録
-bindkey '^G^S' __git_status
