2be341e790ad87e8886c702c214b44bc886346c5
udus122 <u.yudai1202@gmail.com>
2025-06-07 18:45:57 +0900

    feat: add fzf-worktree function for easy navigation between git worktrees

--- .config/zsh/functions.zsh
+++ .config/zsh/functions.zsh
@@ -117,3 +117,46 @@ EOF
 }
 
 alias gn='create_branch_with_guide'
+
+# ref. https://sushichan044.hateblo.jp/entry/2025/06/06/003325
+function fzf-worktree() {
+    # Format of `git worktree list`: path commit [branch]
+    local selected_worktree=$(git worktree list | fzf \
+        --prompt="worktrees > " \
+        --header="Select a worktree to cd into" \
+        --preview="echo '📦 Branch:' && git -C {1} branch --show-current && echo '' && echo '📝 Changed files:' && git -C {1} status --porcelain | head -10 && echo '' && echo '📚 Recent commits:' && git -C {1} log --oneline --decorate -10" \
+        --preview-window="right:40%" \
+        --reverse \
+        --border \
+        --ansi)
+
+    if [ $? -ne 0 ]; then
+        return 0
+    fi
+
+    if [ -n "$selected_worktree" ]; then
+        local selected_path=${${(s: :)selected_worktree}[1]}
+
+        if [ -d "$selected_path" ]; then
+            if zle; then
+                # Called from ZLE (keyboard shortcut)
+                BUFFER="cd ${selected_path}"
+                zle accept-line
+            else
+                # Called directly from command line
+                cd "$selected_path"
+            fi
+        else
+            echo "Directory not found: $selected_path"
+            return 1
+        fi
+    fi
+
+    # Only clear screen if ZLE is active
+    if zle; then
+        zle clear-screen
+    fi
+}
+
+zle -N fzf-worktree
+bindkey '^n' fzf-worktree
