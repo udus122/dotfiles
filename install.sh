674f5ba129c9682e545ce5805710ccbfc35d70be
UD <u.yudai1202@gmail.com>
2025-10-04 09:28:04 +0900

    feat: krewとclaude-codeのネイティブインストールを追加

--- install.sh
+++ install.sh
@@ -6,7 +6,20 @@ script_dir="$(dirname "$0")"
 brew update
 brew bundle install --upgrade --global --cleanup
 
-krew install < "$script_dir/krew.txt"
+( # Install krew
+  cd "$(mktemp -d)" &&
+  OS="$(uname | tr '[:upper:]' '[:lower:]')" &&
+  ARCH="$(uname -m | sed -e 's/x86_64/amd64/' -e 's/\(arm\)\(64\)\?.*/\1\2/' -e 's/aarch64$/arm64/')" &&
+  KREW="krew-${OS}_${ARCH}" &&
+  curl -fsSLO "https://github.com/kubernetes-sigs/krew/releases/latest/download/${KREW}.tar.gz" &&
+  tar zxvf "${KREW}.tar.gz" &&
+  ./"${KREW}" install krew
+)
+
+kubectl krew install < "$script_dir/krew.txt"
 
 # install tools via mise
 (cd "${XDG_CONFIG_HOME}/mise" && mise install)
+
+# Native Install Claude Code
+curl -fsSL https://claude.ai/install.sh | bash
